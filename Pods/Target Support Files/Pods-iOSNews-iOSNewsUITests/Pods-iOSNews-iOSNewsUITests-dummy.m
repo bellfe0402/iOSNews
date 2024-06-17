e341b4833263ddfe9cc5956695bf0f5e073cb9ae
JongCheolSeo <bellfe0402@gmail.com>
2024-06-17 20:46:50 +0900

    feat: Pod install

---
+++ Pods/Target Support Files/Pods-iOSNews-iOSNewsUITests/Pods-iOSNews-iOSNewsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSNews_iOSNewsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_iOSNews_iOSNewsUITests
+@end
---
+++ Pods/Target Support Files/Pods-iOSNews/Pods-iOSNews-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSNews : NSObject
+@end
+@implementation PodsDummy_Pods_iOSNews
+@end
